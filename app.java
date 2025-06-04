37716d4177cfaa9946805baa83c2c6aade0c7fe9
256x652 <sam298325@gmail.com>
2025-06-04 08:55:09 +0530

    update for user print

--- app.java
+++ app.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class App {
+public class app {
     public static void main (String[] args) {
+        System.out.println("Enter a Number :");
         Scanner obj=new Scanner(System.in);
         int x=obj.nextInt();
         for (int i=1; i<=x; i++) {
